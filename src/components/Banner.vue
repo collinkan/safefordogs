5d39addb986e63c042bfd2cfe5aaa0b54f1533dc
Collin Kan <kanc@mcmaster.ca>
2020-08-23 20:32:15 -0400

    added temp logo

---
+++ src/components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img src="./assets/temp_logo.jpeg">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        text-align: center;
+    }
+</style>
