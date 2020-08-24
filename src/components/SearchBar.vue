1e71e2f6479e7628d5e90a3968ec7d312721b53f
Collin Kan <kanc@mcmaster.ca>
2020-08-23 20:56:12 -0400

    Implemented

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input v-model="query" placeholder="Search">
+    </div>    
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            query: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
