27e2c12133d989ff88e3580be2e7886aaa4d5446
Collin Kan <kanc@mcmaster.ca>
2020-08-23 20:37:36 -0400

    imported banner component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-
+    <banner></banner>
   </div>
 </template>
 
 <script>
+  import Banner from './components/Banner.vue'
 
+  export default{
+    components: {
+      banner: Banner
+    }
+  }
 </script>
 
 <style scoped>
