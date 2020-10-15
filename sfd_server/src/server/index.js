a19976cb143ab4ba0046ceffc2bc902b0624dfd1
collinkan <kanc@mcmaster.ca>
2020-10-15 04:20:57 -0400

    Created and initalized sfd_server_test

--- sfd_server/src/server/index.js
+++ sfd_server/src/server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const os = require('os');
-
 const app = express();
+const os = require('os');
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
