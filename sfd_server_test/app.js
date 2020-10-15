659ad05a15806254296e85d2afa5daafd8a89865
collinkan <kanc@mcmaster.ca>
2020-10-15 05:33:18 -0400

    Prevented CORS errors

--- sfd_server_test/app.js
+++ sfd_server_test/app.js
@@ -8,6 +8,15 @@ const item_routes = require('./api/routes/items')
 app.use(body_parser.urlencoded({extended: false}));
 app.use(body_parser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+});
+
 app.use('/items', item_routes);
 
 app.use((req, res, next) => {
