f97daff218fe8e2dab4ae03e959efb7a9108ee6d
collinkan <kanc@mcmaster.ca>
2020-10-15 04:53:10 -0400

    Added routes to items.js

--- sfd_server_test/api/routes/items.js
+++ sfd_server_test/api/routes/items.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Get requests to /items'
+    });
+})
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Post requests to /items'
+    });
+})
+
+router.get('/:itemId', (req, res, next) => {
+    const id = req.params.itemId;
+    res.status(200).json({
+        message: 'Item ID: ' + id,
+    });
+})
+
+module.exports = router
