456a296bd8f2ade21e2723486a223b97733675ea
collinkan <kanc@mcmaster.ca>
2020-10-15 05:12:29 -0400

    Added error handling

--- sfd_server_test/app.js
+++ sfd_server_test/app.js
@@ -6,4 +6,19 @@ const itemRoutes = require('./api/routes/items')
 
 app.use('/items', itemRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
